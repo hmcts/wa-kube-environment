8ddd5fd40579931062d4446fbea697f12ce44e86
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-08-27 14:41:01 +0100

    Added /etc/hosts mapping for services that call themselves via HostAliases. Added .env for variables Added more needed scripts for authentication and role registering.

---
+++ scripts/wiremock.sh
@@ -0,0 +1,273 @@
+#!/usr/bin/env bash
+
+# Setup Wiremock responses for Professional Reference Data based on existing Idam users
+
+share_case_org_code="$(sh ./idam-user-token.sh "${TEST_LAW_FIRM_SHARE_CASE_ORG_USERNAME}" "${TEST_LAW_FIRM_SHARE_CASE_ORG_PASSWORD}")"
+share_case_org_id="$(curl --silent --show-error -X GET "${IDAM_URL}/details" -H "accept: application/json" -H "authorization: Bearer ${share_case_org_code}" | jq -r .id )"
+
+share_case_a_code="$(sh ./idam-user-token.sh "${TEST_LAW_FIRM_SHARE_CASE_A_USERNAME}" "${TEST_LAW_FIRM_SHARE_CASE_A_PASSWORD}")"
+share_case_a_id="$(curl --silent --show-error -X GET "${IDAM_URL}/details" -H "accept: application/json" -H "authorization: Bearer ${share_case_a_code}" | jq -r .id )"
+
+share_case_b_code="$(sh ./idam-user-token.sh "${TEST_LAW_FIRM_SHARE_CASE_B_USERNAME}" "${TEST_LAW_FIRM_SHARE_CASE_B_PASSWORD}")"
+share_case_b_id="$(curl --silent --show-error -X GET "${IDAM_URL}/details" -H "accept: application/json" -H "authorization: Bearer ${share_case_b_code}" | jq -r .id )"
+
+curl -X POST \
+--data '{
+          "request": {
+            "method": "GET",
+            "url": "/refdata/external/v1/organisations/users"
+          },
+          "response": {
+            "status": 200,
+            "headers": {
+              "Content-Type": "application/json"
+            },
+            "jsonBody": {
+              "users": [
+                {
+                  "userIdentifier": "'"${share_case_org_id}"'",
+                  "firstName": "Org Creator",
+                  "lastName": "Legal Rep",
+                  "email": "'"${TEST_LAW_FIRM_SHARE_CASE_ORG_USERNAME}"'",
+                  "roles": [
+                    "caseworker-publiclaw-solicitor",
+                    "pui-case-manager",
+                    "caseworker",
+                    "caseworker-divorce-solicitor",
+                    "caseworker-ia",
+                    "pui-user-manager",
+                    "caseworker-publiclaw",
+                    "caseworker-ia-legalrep-solicitor",
+                    "caseworker-probate-solicitor",
+                    "pui-organisation-manager",
+                    "caseworker-divorce",
+                    "caseworker-divorce-financialremedy",
+                    "prd-admin",
+                    "pui-finance-manager",
+                    "caseworker-probate",
+                    "caseworker-divorce-financialremedy-solicitor"
+                  ],
+                  "idamStatus": "ACTIVE",
+                  "idamStatusCode": "200",
+                  "idamMessage": "11 OK"
+                },
+                {
+                  "userIdentifier": "'"${share_case_a_id}"'",
+                  "firstName": "Share A",
+                  "lastName": "Legal Rep",
+                  "email": "'"${TEST_LAW_FIRM_SHARE_CASE_A_USERNAME}"'",
+                  "roles": [
+                    "caseworker-publiclaw-solicitor",
+                    "pui-case-manager",
+                    "caseworker",
+                    "caseworker-divorce",
+                    "caseworker-divorce-financialremedy",
+                    "caseworker-divorce-solicitor",
+                    "caseworker-probate",
+                    "caseworker-ia",
+                    "caseworker-divorce-financialremedy-solicitor",
+                    "caseworker-publiclaw",
+                    "caseworker-ia-legalrep-solicitor",
+                    "caseworker-probate-solicitor"
+                  ],
+                  "idamStatus": "ACTIVE",
+                  "idamStatusCode": "200",
+                  "idamMessage": "11 OK"
+                },
+                {
+                  "userIdentifier": "'"${share_case_b_id}"'",
+                  "firstName": "Share B",
+                  "lastName": "Legal Rep",
+                  "email": "'"${TEST_LAW_FIRM_SHARE_CASE_B_USERNAME}"'",
+                  "roles": [
+                    "caseworker-publiclaw-solicitor",
+                    "pui-case-manager",
+                    "caseworker",
+                    "caseworker-divorce",
+                    "caseworker-divorce-financialremedy",
+                    "caseworker-divorce-solicitor",
+                    "caseworker-probate",
+                    "caseworker-ia",
+                    "caseworker-divorce-financialremedy-solicitor",
+                    "caseworker-publiclaw",
+                    "caseworker-ia-legalrep-solicitor",
+                    "caseworker-probate-solicitor"
+                  ],
+                  "idamStatus": "ACTIVE",
+                  "idamStatusCode": "200",
+                  "idamMessage": "11 OK"
+                }
+              ]
+            }
+          }
+        }' \
+http://localhost:8991/__admin/mappings/new
+
+curl -X POST \
+--data '{
+    "request": {
+        "method": "GET",
+        "url": "/health"
+    },
+    "response": {
+        "status": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "jsonBody": {
+            "status": "UP",
+            "components": {
+                "db": {
+                    "status": "UP",
+                    "details": {
+                        "database": "PostgreSQL",
+                        "result": 1,
+                        "validationQuery": "SELECT 1"
+                    }
+                },
+                "diskSpace": {
+                    "status": "UP",
+                    "details": {
+                        "total": 33685291008,
+                        "free": 9184305152,
+                        "threshold": 10485760
+                    }
+                },
+                "hystrix": {
+                    "status": "UP"
+                },
+                "liveness": {
+                    "status": "UP"
+                },
+                "ping": {
+                    "status": "UP"
+                },
+                "refreshScope": {
+                    "status": "UP"
+                },
+                "serviceAuth": {
+                    "status": "UP"
+                }
+            }
+        }
+    }
+}' \
+http://localhost:8991/__admin/mappings/new
+
+# fee-register response for fee with hearinng
+curl -X POST \
+--data '{
+    "request": {
+        "method": "GET",
+        "url": "/fees-register/fees/lookup?channel=default&event=issue&jurisdiction1=tribunal&jurisdiction2=immigration%20and%20asylum%20chamber&keyword=ABC&service=other"
+    },
+    "response": {
+        "status": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "jsonBody": {
+            "code": "FEE0238",
+            "description": "Appeal determined with a hearing",
+            "version": 2,
+            "fee_amount": "140.00"
+        }
+    }
+}' \
+http://localhost:8991/__admin/mappings/new
+
+# fee-register response for fee without hearing
+curl -X POST \
+--data '{
+    "request": {
+        "method": "GET",
+        "url": "/fees-register/fees/lookup?channel=default&event=issue&jurisdiction1=tribunal&jurisdiction2=immigration%20and%20asylum%20chamber&keyword=DEF&service=other"
+    },
+    "response": {
+        "status": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "jsonBody": {
+            "code": "FEE0456",
+            "description": "Appeal determined with a hearing",
+            "version": 2,
+            "fee_amount": "80.00"
+        }
+    }
+}' \
+http://localhost:8991/__admin/mappings/new
+
+curl -X POST \
+--data '{
+          "request": {
+            "method": "POST",
+            "url": "/credit-account-payments",
+            "headers": {
+              "Content-Type": {
+                "equalTo": "application/json"
+              }
+            }
+          },
+          "response": {
+            "status": 201,
+            "headers": {
+              "Content-Type": "application/json"
+            },
+            "jsonBody": {
+                "reference": "RC-1590-6786-1063-9996",
+                "date_created": "2020-05-28T15:10:10.694+0000",
+                "status": "Success",
+                "payment_group_reference": "2020-1590678609071",
+                "status_histories": [
+                  {
+                    "status": "success",
+                    "date_created": "2020-05-28T15:10:10.700+0000",
+                    "date_updated": "2020-05-28T15:10:10.700+0000"
+                  }
+                ]
+            }
+          }
+        }' \
+http://localhost:8991/__admin/mappings/new
+
+#PBA accounts
+curl -X POST \
+--data '{
+          "request": {
+            "method": "GET",
+            "urlPath": "/refdata/external/v1/organisations/pbas"
+          },
+          "response": {
+            "status": 200,
+            "headers": {
+              "Content-Type": "application/json"
+            },
+            "jsonBody": {
+              "organisationEntityResponse" : {
+                "organisationIdentifier": "0UFUG4Z",
+                "name": "ia-legal-rep-org",
+                "status": "ACTIVE",
+                "sraId": null,
+                "sraRegulated": false,
+                "companyNumber": null,
+                "companyUrl": null,
+                "superUser": {
+                  "firstName": "legalrep",
+                  "lastName": "orgcreator",
+                  "email": "'"${TEST_LAW_FIRM_SHARE_CASE_ORG_USERNAME}"'"
+                },
+                "paymentAccount": [
+                  "PBA1234567",
+                  "PBA7654321",
+                  "PBA1232123"
+                ],
+                "contactInformation": null
+              }
+            }
+          }
+        }' \
+http://localhost:8991/__admin/mappings/new
+
+# make responses persistent in Docker volume
+curl -X POST http://localhost:8991/__admin/mappings/save
