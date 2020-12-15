8eba00f915adb45435f2c987c31c8cd960398dc9
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2020-12-15 16:47:30 +0000

    automated DMN BPMN script (#21)
    * automated DMN BPMN script

---
+++ scripts/camunda-deployment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+## Usage: ./camunda-deployment
+##
+## Options:
+##    - SERVICE_TOKEN: which is generated with the idam-service-token.
+##
+## deploys bpmn/dmn to camunda.
+
+SERVICE_TOKEN="$(sh ./actions/idam-service-token.sh "wa_camunda_pipeline_upload")"
+
+echo "Uploading Camunda BPMs and DMNs..."
+if [[ -z "${WA_BPMNS_DMNS_PATH}" ]]; then
+  echo "Environment variable WA_BPMNS_DMNS_PATH was not set skipping deployment."
+else
+  echo "Deploying WA Standalone Task BPMN and DMNs"
+  $WA_BPMNS_DMNS_PATH/camunda-deployment.sh $SERVICE_TOKEN
+
+fi
+
+if [[ -z "${IA_TASK_DMNS_BPMNS_PATH}" ]]; then
+  echo "Environment variable IA_TASK_DMNS_BPMNS_PATH was not set skipping deployment."
+else
+  echo "Deploying IA Task Configuration BPMN and DMNs"
+  $IA_TASK_DMNS_BPMNS_PATH/camunda-deployment.sh $SERVICE_TOKEN
+fi
