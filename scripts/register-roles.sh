64fffbc80a268c05e51a84239bf93462b4560352
David Crespo <david.crespo@hmcts.net>
2021-06-01 13:53:40 +0100

    add new IA role (#54)

--- scripts/register-roles.sh
+++ scripts/register-roles.sh
@@ -17,6 +17,7 @@ SERVICE_TOKEN="$(sh ./actions/idam-service-token.sh)"
 ./actions/register-role.sh "caseworker-ia-respondentofficer" "$USER_TOKEN" "$SERVICE_TOKEN"
 ./actions/register-role.sh "caseworker-ia-iacjudge" "$USER_TOKEN" "$SERVICE_TOKEN"
 ./actions/register-role.sh "caseworker-wa-task-configuration" "$USER_TOKEN" "$SERVICE_TOKEN"
+./actions/register-role.sh "caseworker-ia-srcaseofficer" "$USER_TOKEN" "$SERVICE_TOKEN"
 
 ./actions/register-role.sh "caseworker-ras-validation" "$USER_TOKEN" "$SERVICE_TOKEN"
 ./actions/register-role.sh "caseworker-caa" "$USER_TOKEN" "$SERVICE_TOKEN"
