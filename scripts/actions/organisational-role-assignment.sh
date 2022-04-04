df44ba0162fcad27a1c609863e02df0ba29a97e0
dharmendra kumar <dharmendra.kumar@hmcts.net>
2022-04-04 10:37:59 +0100

    Update Role assignment to factor in new changes (#101)
    * Update Role assignment to factor in new changes
    
    * add task supervisor for user
    
    * add org role for test wa las from

--- scripts/actions/organisational-role-assignment.sh
+++ scripts/actions/organisational-role-assignment.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 ## Usage: ./organisational-role-assignment.sh [username] [password] [role_classification] [role_name] [role_attributes] [microservice_name]
 ##
 ## Options:
@@ -15,7 +15,7 @@ PASSWORD=${2:-London01}
 ROLE_CLASSIFICATION="${3:-PUBLIC}"
 ROLE_NAME="${4:-"tribunal-caseworker"}"
 ROLE_ATTRIBUTES="${5:-'{"jurisdiction":"IA"}'}"
-MICROSERVICE="${6:-ccd_gw}"
+MICROSERVICE="${6:-wa_task_management_api}"
 
 BASEDIR=$(dirname "$0")
 
@@ -23,6 +23,7 @@ USER_TOKEN=$($BASEDIR/idam-user-token.sh $USERNAME $PASSWORD)
 USER_ID=$($BASEDIR/idam-user-id.sh $USER_TOKEN)
 SERVICE_TOKEN=$($BASEDIR/idam-service-token.sh $MICROSERVICE)
 
+echo "\n\nCreating role assignment: \n User: ${USER_ID}\n Role name: ${ROLE_NAME}\n ROLE_CLASSIFICATION: ${ROLE_CLASSIFICATION}\n"
 
 curl --silent --show-error -X POST "${ROLE_ASSIGNMENT_URL}/am/role-assignments" \
   -H "accept: application/vnd.uk.gov.hmcts.role-assignment-service.create-assignments+json;charset=UTF-8;version=1.0" \
