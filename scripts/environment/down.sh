1c523bd27be932bacee24d77213f3b432dcbfb06
rafalkalita <rkalitainfo@gmail.com>
2022-11-15 17:14:07 +0000

    RWA-1833 - working new DB in the same postgres cluster

--- scripts/environment/down.sh
+++ scripts/environment/down.sh
@@ -6,5 +6,11 @@ SECRET_NAME="hmcts-private-creds"
 echo "💣  Deleting ACR token"
 kubectl delete secret $SECRET_NAME -n $NAMESPACE
 
+echo "💣  Deleting persistent volume claims"
+kubectl delete -n $NAMESPACE pvc --all
+
+echo "💣  Deleting persistentvolumes"
+kubectl delete -n $NAMESPACE pv --all
+
 echo "💣  Stopping and removing all containers"
 helmfile -n $NAMESPACE destroy
