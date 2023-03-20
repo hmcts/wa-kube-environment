1642969673208c8eb448363d819944ea4458b14d
vasu <vasu.kaveri@hmcts.net>
2023-03-20 13:01:04 +0000

    DTSTM-97: mac setup for wa-pet team

--- scripts/actions/idam-user-token.sh
+++ scripts/actions/idam-user-token.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ## Usage: ./idam-user-token.sh [user] [password]
 ##
 ## Options:
--- scripts/user-details.sh
+++ scripts/user-details.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ## Usage: ./user-details.sh [username] [password]
 ##
 ## Options:
