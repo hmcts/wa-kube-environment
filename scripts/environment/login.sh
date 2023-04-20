4cddd461156d22b3206c4dcd5cde5dafa86ff731
Chris Davidson <christopher.davidson@btinternet.com>
2023-04-20 14:15:21 +0100

    Rwa 1834 (#121)
    * RWA-1833 - working new DB in the same postgres cluster
    
    * RWA-1834 - pg_hba.conf, connections and repl_user
    
    * RWA 1834 updates for new database
    
    * updated merge
    
    * RWA 1834 fix for setting wal_level
    
    * undo max replication slots
    
    * Upped max connections
    
    * RWA 1834 Added superuser and connects
    
    * RWA 2182 added container for replica database
    
    * RWA 1834 updated readme
    
    * Upgrade postgres to 14
    
    ---------
    
    Co-authored-by: rafalkalita <rkalitainfo@gmail.com>

--- scripts/environment/login.sh
+++ scripts/environment/login.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 echo "-> Performing Login"
-TOKEN=$(az acr login --name hmctsprivate --subscription DCD-CNP-PROD --expose-token | jq --raw-output '.accessToken')
+TOKEN_PRIVATE=$(az acr login --name hmctsprivate --subscription DCD-CNP-PROD --expose-token | jq --raw-output '.accessToken')
+TOKEN_PUBLIC=$(az acr login --name hmctspublic --subscription DCD-CNP-PROD --expose-token | jq --raw-output '.accessToken')
 
-
- if docker login hmctsprivate.azurecr.io --username=00000000-0000-0000-0000-000000000000 --password=$TOKEN > /dev/null ; then
-     echo "✅  Logged in successfully"
+ if docker login hmctsprivate.azurecr.io --username=00000000-0000-0000-0000-000000000000 --password=$TOKEN_PRIVATE > /dev/null ; then
+     echo "✅  Logged in successfully to hmctsprivate.azurecr.io"
    else
-     echo "❌  Something went wrong when performing the log in"
+     echo "❌  Something went wrong when performing the log in to hmctsprivate.azurecr.io"
+   fi
+
+ if docker login hmctspublic.azurecr.io --username=00000000-0000-0000-0000-000000000000 --password=$TOKEN_PUBLIC > /dev/null ; then
+     echo "✅  Logged in successfully to hmctspublic.azurecr.io"
+   else
+     echo "❌  Something went wrong when performing the log in to hmctspublic.azurecr.io"
    fi
