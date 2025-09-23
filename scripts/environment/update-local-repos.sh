3c02307c05ff44d9adf1514207b2613598402426
Martin <martin.spasov@hmcts.net>
2025-09-23 13:59:42 +0100

    Small fixes

--- scripts/environment/update-local-repos.sh
+++ scripts/environment/update-local-repos.sh
@@ -3,23 +3,12 @@ set -e
 
 echo "✅ Updating repos"
 
-PROJECT_PATH="<the_path_to_your_projects>"
+PROJECT_PATH="/Users/martinspasov/Dev"
 
 CMD='git checkout master && git pull --ff-only origin master && git gc'
-CMD_INTEL='git checkout kube-env-mac_intel_chips && git pull --ff-only origin kube-env-mac_intel_chips && git gc'
 
-# wa-kube-environment uses the intel branch
-repo="${PROJECT_PATH}/wa-kube-environment"
-if [ -d "$repo" ]; then
-  echo "📦 $(basename "$repo")"
-  (cd "$repo" && eval "$CMD_INTEL")
-else
-  echo "⚠️  Missing: $repo"
-fi
 
-# The rest use master
 for r in \
-  wa-kube-environment \
   wa-case-event-handler \
   wa-post-deployment-ft-tests \
   wa-standalone-task-bpmn \
